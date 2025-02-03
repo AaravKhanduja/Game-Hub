fc69bccbb161fa2318247da447097bca4f0f4062
AaravKhanduja <aaravkhanduja@gmail.com>
2025-02-03 17:06:19 -0500

    Install Chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 import { useRef } from 'react'
 import axios from 'axios';
- 
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
 
  function App() { 
     
@@ -9,7 +10,10 @@ import axios from 'axios';
   
   
   return (  
-    <>  
+    <>   
+    <Button colorPalette = 'blue' >Button</Button>
+
+
     </>
   );
 

7abd60690af1508c86605cc7a0853a11d93c0cb5
AaravKhanduja <aaravkhanduja@gmail.com>
2025-02-03 18:45:40 -0500

    fixed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Grid, GridItem, useBreakpointValue } from '@chakra-ui/react';
 import NavBar from './components/navbar/navbar';
-
 function App() {
   const gridTemplate = useBreakpointValue({
     base: `"nav" "main"`, 
-    lg: `"nav main" "aside main"`
+    lg: `"nav nav" "aside main"`
   }) ?? `"nav" "main"`;  // Fallback to prevent 'undefined'
 
   return (
     <Grid templateAreas={gridTemplate} gap={4}>
-      <GridItem area="nav" >
-      <NavBar /></GridItem>
+      <GridItem area="nav" bg="coral">
+        <NavBar />
+      </GridItem>
+       
 
       {gridTemplate.includes("aside") && (
         <GridItem area="aside" bg="gold">Aside</GridItem>

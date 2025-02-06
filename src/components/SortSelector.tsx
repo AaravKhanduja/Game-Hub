dd2b405c513384552b81a136b934585a0e4756a0
AaravKhanduja <aaravkhanduja@gmail.com>
2025-02-06 02:07:02 -0500

    index html title updated, sort selector filter name removed.

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -12,8 +12,7 @@ const SortSelector = ({onSelectSort}: Props) => {
 
      const sortOrders = [
           {value: '', label: 'Relevance'},
-          {value: '-added', label: 'Date added'},
-          {value: 'name', label: 'Name'},
+          {value: '-added', label: 'Date added'}, 
           {value: '-released', label: 'Release date'},
           {value: '-metacritic', label: 'Popularity'}
 

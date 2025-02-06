b2b5ad6f84ff40f448d22d42324a4531edc73638
AaravKhanduja <aaravkhanduja@gmail.com>
2025-02-05 22:47:23 -0500

    vercel tsconfig optimized to solve deployment bug.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Grid, GridItem, HStack, useBreakpointValue } from '@chakra-ui/react';
-import NavBar from './components/navbar/navbar';
-import GameGrid from './components/GameGrid';
-import SideBar from './components/SideBar';
-import { Genre } from './hooks/useGenres';
-import { useState } from 'react';
-import PlatformSelector from './components/PlatformSelector';
-import { Platform } from './hooks/usePlatforms';
-import SortSelector from './components/SortSelector';
-import GameHeading from './components/GameHeading';
-import { Box } from '@chakra-ui/react';
+import { Grid, GridItem, HStack, useBreakpointValue } from "@chakra-ui/react";
+import NavBar from "./components/navbar/NavBar";
+import GameGrid from "./components/GameGrid";
+import SideBar from "./components/SideBar";
+import { Genre } from "./hooks/useGenres";
+import { useState } from "react";
+import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/usePlatforms";
+import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
+import { Box } from "@chakra-ui/react";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -17,43 +17,60 @@ export interface GameQuery {
   searchText: string;
 }
 function App() {
-  const [gameQuery, setGameQuery] = useState<GameQuery>({genre: null, platform: null, sortOrder: '', searchText: ''});
+  const [gameQuery, setGameQuery] = useState<GameQuery>({
+    genre: null,
+    platform: null,
+    sortOrder: "",
+    searchText: "",
+  });
 
-  const gridTemplate = useBreakpointValue({
-    base: `"nav" "main"`, 
-    lg: `"nav nav" "aside main"`
-  }) ?? `"nav" "main"`;  // Fallback to prevent 'undefined'
+  const gridTemplate =
+    useBreakpointValue({
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`,
+    }) ?? `"nav" "main"`; // Fallback to prevent 'undefined'
 
   return (
-    <Grid templateAreas={gridTemplate}  
+    <Grid
+      templateAreas={gridTemplate}
       templateColumns={{
-        base: '1fr',
-        lg: '200px 1fr'
-        
-        }}>
-      <GridItem area="nav" >
-        <NavBar onSearch={(searchText)=> setGameQuery({...gameQuery, searchText })} />
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
-       
 
       {gridTemplate.includes("aside") && (
-        <GridItem paddingX={5} area="aside"><SideBar onSelectGenre={(genre)=> setGameQuery({...gameQuery, genre})}/></GridItem>
+        <GridItem paddingX={5} area="aside">
+          <SideBar
+            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+          />
+        </GridItem>
       )}
-       
-      
 
-      <GridItem area="main" >
-        <Box paddingLeft='9'>
-        <GameHeading gameQuery={gameQuery} />
-        <HStack >
-      <PlatformSelector onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})} />
-        <SortSelector onSelectSort={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
-      </HStack>
-      </Box>
-      
-      <GameGrid gameQuery={gameQuery} />
+      <GridItem area="main">
+        <Box paddingLeft="9">
+          <GameHeading gameQuery={gameQuery} />
+          <HStack>
+            <PlatformSelector
+              onSelectPlatform={(platform) =>
+                setGameQuery({ ...gameQuery, platform })
+              }
+            />
+            <SortSelector
+              onSelectSort={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+            />
+          </HStack>
+        </Box>
+
+        <GameGrid gameQuery={gameQuery} />
       </GridItem>
-      
     </Grid>
   );
 }
